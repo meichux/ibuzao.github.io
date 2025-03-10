ccd7ce84b9a699c11c05633d51035e8ce898f2fd
unknown <xxx>
2025-03-10 18:57:21 +0800

    Site updated: 2025-03-10 18:57:15

---
+++ anzhiyu/random.js
@@ -0,0 +1,3 @@
+var posts=["2025/03/08/关于行动力不足的思考/","2025/03/08/大脑记忆能力减弱/","2025/03/09/写给迷茫的自己/","2025/03/08/yinsi/"];function toRandomPost(){
+    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+  };
