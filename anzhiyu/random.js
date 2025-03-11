9085402d26e887887c2911ec31355515b1132590
unknown <xxx>
2025-03-11 22:24:15 +0800

    Site updated: 2025-03-11 22:24:13

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/5634cdad.html","posts/269d3d81.html","posts/351ffcb9.html","posts/c27aca5d.html","posts/61622d0e.html"];function toRandomPost(){
+var posts=["posts/5634cdad.html","posts/c27aca5d.html","posts/351ffcb9.html","posts/269d3d81.html","posts/61622d0e.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
