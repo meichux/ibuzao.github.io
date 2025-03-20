3ce0054aab4138b0018a6a42d1f0093ad18cc6db
unknown <xxx>
2025-03-20 14:52:54 +0800

    Site updated: 2025-03-20 14:52:53

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/5634cdad.html","posts/269d3d81.html","posts/ccff787.html","posts/351ffcb9.html","posts/61622d0e.html","posts/c27aca5d.html"];function toRandomPost(){
+var posts=["posts/269d3d81.html","posts/5634cdad.html","posts/ccff787.html","posts/351ffcb9.html","posts/61622d0e.html","posts/c27aca5d.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
