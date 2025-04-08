55b60d5bf3454f143da702f1cf96ae3922e0dab2
unknown <xxx>
2025-04-08 16:38:57 +0800

    Site updated: 2025-04-08 16:38:52

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/cb6ed29f.html","posts/5634cdad.html","posts/269d3d81.html","posts/ccff787.html","posts/c27aca5d.html","posts/424a780e.html","posts/351ffcb9.html","posts/61622d0e.html"];function toRandomPost(){
+var posts=["posts/5634cdad.html","posts/cb6ed29f.html","posts/424a780e.html","posts/351ffcb9.html","posts/269d3d81.html","posts/ccff787.html","posts/c27aca5d.html","posts/61622d0e.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
